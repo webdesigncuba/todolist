6fa54a7bbbc972695fc8b0483e9cd8b4e57b2c85
David <webdesigncuba@gmail.com>
2023-11-25 01:38:06 -0300

    Adicionado bootstrap e estilos ao frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NgbModule,
     
   ],
   providers: [],
